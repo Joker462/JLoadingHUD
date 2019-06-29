c171709f788dd0225eed0ef1005402c6d0fb6e13
Hung <hungthai270893@gmail.com>
2019-06-29 16:49:15 +0700

    add example folder

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Hung Thai Minh on 6/29/19.
+//  Copyright © 2019 Hung Thai Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
